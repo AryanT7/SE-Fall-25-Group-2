51681f50a13ad3fbe108ddbaa2a44a721110b3ed
Supraj Gijre <suprajgijre@gmail.com>
2025-10-17 16:14:42 -0400

    Updated CORS and library versions

--- proj2/backend/app/main.py
+++ proj2/backend/app/main.py
@@ -18,11 +18,17 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Cafe Calories API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(auth_router.router)
 app.include_router(users_router.router)
